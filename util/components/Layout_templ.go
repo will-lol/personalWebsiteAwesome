7a920bf66db15113f2b84461c423bae9331d3480
will <will.bradshaw50@gmail.com>
2024-01-28 12:49:01 +1100

    begin work on footer

--- util/components/Layout_templ.go
+++ util/components/Layout_templ.go
@@ -48,6 +48,10 @@ func Layout(title string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
+		templ_7745c5c3_Err = Footer().Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</body></html>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
