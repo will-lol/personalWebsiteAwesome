4518434fe3c643e284a01af27d94b4146d2197fe
will <will.bradshaw50@gmail.com>
2024-02-03 14:55:17 +1100

    add carousel button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './**/*.{templ,css}',
+    './**/*.{templ,css,js}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '0.5rem',
+    },
+    screens: {
+      "sm": "640px",
+      "md": "768px",
+    },
     extend: {
       colors: {
         warm: {
           100: "oklch(98.23% 0.0152 67.74)",
-          200: "oklch(92% 0.0152 67.74)",
-          300: "oklch(85% 0.0152 67.74)",
+          200: "oklch(96% 0.0152 67.74)",
+          300: "oklch(92% 0.0152 67.74)",
           400: "oklch(80% 0.0152 67.74)",
           500: "oklch(70% 0.0212 67.74)",
           600: "oklch(60% 0.0212 67.74)",
@@ -20,6 +28,7 @@ module.exports = {
       },
       backgroundImage: ({ theme }) => ({
         "noise": "url(\"/assets/images/noise.png\")",
+        "vignette": "radial-gradient(transparent, rgba(0,0,0,0.3))"
       }),
     },
     fontFamily: {
@@ -28,7 +37,7 @@ module.exports = {
     },
   },
   plugins: [
-    
+
   ],
   corePlugins: {
     preflight: true
