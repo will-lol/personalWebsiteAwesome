85fbb5e3788074b6420a6ac8912661e73d063343
Will <will.bradshaw50@gmail.com>
2024-02-19 21:21:26 +1100

    start work on blog UI

--- go.mod
+++ go.mod
@@ -18,7 +18,9 @@ require (
 )
 
 require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/Masterminds/semver/v3 v3.2.1 // indirect
+	github.com/adrg/frontmatter v0.2.0 // indirect
 	github.com/aws/aws-lambda-go v1.37.0 // indirect
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.0 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.16.13 // indirect
@@ -45,12 +47,16 @@ require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/kaleocheng/goldmark v1.1.10 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/yuin/goldmark v1.7.0 // indirect
+	go.abhg.dev/goldmark/frontmatter v0.1.0 // indirect
 	golang.org/x/crypto v0.18.0 // indirect
 	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
 	golang.org/x/mod v0.14.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/tools v0.16.1 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- routes/blog/default.go
+++ routes/blog/default.go
@@ -8,30 +8,46 @@ import (
 	blogService "github.com/will-lol/personalWebsiteAwesome/services/blog"
 )
 
-type BlogHandler struct {
-	Files       *blogService.Files
-	Log         *slog.Logger
-	blogService *blogService.Blog
+type blogHandler struct {
+	FilesDependency *blogService.Files
+	Log             *slog.Logger
+	blogService     blogService.Blog
 }
 
-func NewBlogHandler(f *blogService.Files, l *slog.Logger) (*BlogHandler, error) {
+func NewBlogHandler(f *blogService.Files, l *slog.Logger) (*blogHandler, error) {
 	bService, err := blogService.NewBlog(*f)
 	if err != nil {
 		return nil, err
 	}
 
-	return &BlogHandler{
-		Files: f,
-		Log:   l,
-		blogService: bService,
+	return &blogHandler{
+		FilesDependency: f,
+		Log:             l,
+		blogService:     *bService,
 	}, nil
 }
 
-func (b BlogHandler) blogHandler(w http.ResponseWriter, r *http.Request) {
-	component := blog()
+func (b blogHandler) blogHandler(w http.ResponseWriter, r *http.Request) {
+	component := blog(b.blogService)
 	component.Render(r.Context(), w)
 }
 
-func (b BlogHandler) Router(r chi.Router) {
-	r.Get("/", b.blogHandler)
+func (b blogHandler) postHandler(w http.ResponseWriter, r *http.Request) {
+	slug := chi.URLParam(r, "slug")
+
+	blogPost, err := b.blogService.GetPost(slug)
+	if err != nil {
+		w.WriteHeader(404)
+		w.Write([]byte("404 Not Found"))
+		return
+	}
+
+	component := postPage(blogPost)
+	component.Render(r.Context(), w)
 }
+
+func (b blogHandler) Router(r chi.Router) {
+	r.Get("/", b.blogHandler)
+	r.Get("/{slug}", b.postHandler)
+}
+
