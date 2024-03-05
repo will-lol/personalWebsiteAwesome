cf40c1414ad96cdcb22c4e9d907337d5cc625e70
Will <will.bradshaw50@gmail.com>
2024-03-05 19:27:29 +1100

    add url in h-post

--- services/url/default.go
+++ services/url/default.go
@@ -1,9 +1,10 @@
 package url
 
 import (
-	"net/url"
 	"context"
 	"errors"
+	"log/slog"
+	"net/url"
 )
 
 func GetURL(ctx context.Context) (*url.URL, error) {
@@ -11,4 +12,12 @@ func GetURL(ctx context.Context) (*url.URL, error) {
 		return req, nil
 	}
 	return nil, errors.New("couldn't get url")
+}
+
+func GetURLHandled(ctx context.Context) (*url.URL) {
+	if req, ok := ctx.Value("url").(*url.URL); ok {
+		return req
+	}
+	slog.Default().Error("couldn't get url")
+	return nil
 }
