ddee0dfa432bf438a7c02b983977afcfdc8f6bf3
will <will.bradshaw50@gmail.com>
2024-04-27 16:30:48 +1000

    migrate to nixpkgs cdk

--- flake.nix
+++ flake.nix
@@ -5,26 +5,13 @@
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
     templRepo.url = "github:a-h/templ/refs/tags/v0.2.543";
-    aws-cdk = {
-      url = "https://registry.npmjs.org/aws-cdk/-/aws-cdk-2.139.0.tgz";
-      flake = false;
-    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, aws-cdk, templRepo }:
+  outputs = { self, nixpkgs, flake-utils, templRepo }:
     flake-utils.lib.eachDefaultSystem (system: 
       let 
         name = "personalWebsite";
 	templ = templRepo.packages.${system}.templ;
-	cdk = pkgs.stdenv.mkDerivation {
-	  name = "aws-cdk";
-	  version = "2.121.1";
-	  src = aws-cdk;
-	  phases = [ "installPhase" "patchPhase" ];
-	  installPhase = ''
-	    cp -r $src $out
-	  '';
-	};
 	overlays = [];
 	lib = nixpkgs.lib;
 	pkgs = import nixpkgs { inherit system overlays; };
@@ -45,7 +32,7 @@
 	  };
 	  defaultPackage = goBuild;
 	  devShell = pkgs.mkShell {
-	    packages = [ pkgs.tailwindcss pkgs.entr pkgs.nodePackages.typescript-language-server pkgs.fd pkgs.nodejs_18 pkgs.awscli2 pkgs.aws-sam-cli pkgs.go pkgs.gopls templ cdk ];
+	    packages = [ pkgs.tailwindcss pkgs.entr pkgs.nodePackages.typescript-language-server pkgs.fd pkgs.nodejs_18 pkgs.awscli2 pkgs.aws-sam-cli pkgs.go pkgs.gopls pkgs.nodePackages.aws-cdk templ ];
 	    shellHook = ''
 	      export ENVIRONMENT=dev
 	    '';
